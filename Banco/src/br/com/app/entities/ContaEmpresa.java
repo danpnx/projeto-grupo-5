b50727487d3100ab48a116e96366d32d14af4889
daniel augusto <danielpn23@outlook.com>
2022-07-25 20:32:14 -0300

    add atributo que contem valor de emprestimo da conta empresa

--- Banco/src/br/com/app/entities/ContaEmpresa.java
+++ Banco/src/br/com/app/entities/ContaEmpresa.java
@@ -1,8 +1,16 @@
 package br.com.app.entities;
 
 public class ContaEmpresa {
+	
+	private Double emprestimoEmpresa = new Double(10_000); 
 
-	double emprestimoEmpresa;
+	public Double getEmprestimoEmpresa() {
+		return emprestimoEmpresa;
+	}
+
+	public void setEmprestimoEmpresa(Double emprestimoEmpresa) {
+		this.emprestimoEmpresa = emprestimoEmpresa;
+	}
 
 	public void pedirEmprestimo() {
 	}
