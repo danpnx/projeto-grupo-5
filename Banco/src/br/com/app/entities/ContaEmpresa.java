a277ae4b652ca08e62b883881ecb8f74522db51a
Marcelo M. Pimenta <101780975+marcelompimenta@users.noreply.github.com>
2022-07-25 21:05:20 -0400

    Atualizado para o autor
    Inseri o autor dos códigos acima.

--- Banco/src/br/com/app/entities/ContaEmpresa.java
+++ Banco/src/br/com/app/entities/ContaEmpresa.java
@@ -1,19 +1,20 @@
+// Daniel
 package br.com.app.entities;
-
+// Daniel
 import br.com.app.conta.Conta;
-
+// Daniel
 public class ContaEmpresa extends Conta{
-	
+	// Daniel
 	private Double emprestimoEmpresa = new Double(10_000); 
-
+	// Daniel
 	public Double getEmprestimoEmpresa() {
 		return emprestimoEmpresa;
 	}
-
+	// Daniel
 	public void setEmprestimoEmpresa(Double emprestimoEmpresa) {
 		this.emprestimoEmpresa = emprestimoEmpresa;
 	}
-
+	// Daniel
 	public void pedirEmprestimo(double valorEmprestimo) {
 		if(valorEmprestimo <= this.emprestimoEmpresa) {
 			this.emprestimoEmpresa -= valorEmprestimo;
@@ -22,7 +23,7 @@ public class ContaEmpresa extends Conta{
 			System.out.println("Impossível realizar o empréstimo. Saldo de empréstimo disponível: R$ " + this.emprestimoEmpresa);
 		}
 	}
-	
+	// Daniel
 	@Override
 	public void debitarConta(double valorDebitado) {
 		this.setSaldoConta(this.getSaldoConta() - valorDebitado);
