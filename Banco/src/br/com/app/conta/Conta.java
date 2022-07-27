623608d426ae5a08886683a8b9daa892173ec49c
daniel <danielpn23@outlook.com>
2022-07-27 13:21:37 -0300

    adiciona arraylist de movimentos bancarios e adiciona movimento bancario na lista

--- Banco/src/br/com/app/conta/Conta.java
+++ Banco/src/br/com/app/conta/Conta.java
@@ -1,29 +1,38 @@
 package br.com.app.conta;
 
+import java.util.ArrayList;
+
 public abstract class Conta {
 	
-	// Inicio do codigo Daniel
-	
+	// Daniel
 	private String cpfConta;
 	private int numeroConta;
 	private double saldoConta;
 	private boolean contaAtiva = false;
+	private ArrayList<MovimentoBancario> extratoMovimentoBancario = new ArrayList<>();
 	
+	// Daniel
 	public String getCpfConta() {
 		return cpfConta;
 	}
+	// Daniel
 	public void setCpfConta(String cpfConta) {
 		this.cpfConta = cpfConta;
 	}
+	// Daniel
 	public int getNumeroConta() {
 		return numeroConta;
 	}
+	// Daniel
 	public void setNumeroConta(int numeroConta) {
 		this.numeroConta = numeroConta;
 	}
+	// Daniel
 	public double getSaldoConta() {
 		return saldoConta;
 	}
+	
+	// Daniel
 	public void setSaldoConta(double novoSaldo) {
 		if(this.contaAtiva) {
 			this.creditarConta(saldoConta);
@@ -32,18 +41,24 @@ public abstract class Conta {
 			this.saldoConta = novoSaldo;
 			System.out.println("Utilizando a referência pro atributo this.saldoConta");
 		}
-		// this.saldoConta = saldoConta;
 	}
+	// Daniel
 	public boolean isContaAtiva() {
 		return contaAtiva;
 	}
+	// Daniel
 	public void setContaAtiva(boolean contaAtiva) {
 		this.contaAtiva = contaAtiva;
 	}
+	
+	// Daniel
 	public abstract void debitarConta(double valorDebitado);
 	
+	// Daniel
 	public void creditarConta(double valorCreditado){
 		this.saldoConta += valorCreditado;
+		
+		// Adiciona transação bancária ao extrato
+		this.extratoMovimentoBancario.add(new MovimentoBancario(valorCreditado, "Crédito"));
 	}
-	// Fim do codigo Daniel
 }
