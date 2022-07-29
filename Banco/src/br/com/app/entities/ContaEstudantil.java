e3cf5cf14de440a28ab48375e1ce10c7ea8eedc9
riziaraquelbra <riziaraquelbra@gmail.com>
2022-07-28 23:28:42 -0300

    Alterações inseridas na classe Conta Estudantil, criação de métodos e atributos.

--- Banco/src/br/com/app/entities/ContaEstudantil.java
+++ Banco/src/br/com/app/entities/ContaEstudantil.java
@@ -1,25 +1,38 @@
 package br.com.app.entities;
 
-import br.com.app.conta.Conta;
-
 public class ContaEstudantil extends Conta{
-	private double limiteEstudantil;
+    private double limiteEstudantil = 5000;
+    private double saldoConta;
 
-	public double getLimiteEstudantil() {
-		return limiteEstudantil;
-	}
+    public void setSaldoConta(double novoSaldo) {
+        this.saldoConta = novoSaldo;
+    }
 
-	public void setLimiteEstudantil(double limiteEstudantil) {
-		this.limiteEstudantil = limiteEstudantil;
-	}
+    public double getSaldoConta() {
+        return this.saldoConta;
+    }
 
-	public void usarEstudantil() {
-	}
 
-	@Override
-	public void debitarConta(double valorDebitado) {
-		// TODO Auto-generated method stub
-		
-	}
+       public double getLimiteEstudantil() {
+           return this.limiteEstudantil;
+       }
+    public void setLimiteEstudantil(double limiteEstudantil) {
+        this.limiteEstudantil = limiteEstudantil;
+    }
+
+    public void pagarDebito(double valorDebito) {
+        if(this.saldoConta >= valorDebito) {
+            this.saldoConta -= valorDebito;
+            System.out.println("Pagamento efetuado. ");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+    public void usarEstudantil(double valorEmprestimo) {
+
+        this.saldoConta = this.saldoConta + valorEmprestimo;
+        this.limiteEstudantil = this.limiteEstudantil - valorEmprestimo;
+        System.out.println("Empréstimo efetuado.");
+    }
 
 }
